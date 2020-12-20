6691d24b05178431f7b839d71ae8e1787aa4bf1e
Allan Laal <allan.laal@gmail.com>
2020-12-20 16:22:08 +0200

    RENAME to user.js

--- hide-both-sidebars.user.js
+++ hide-both-sidebars.user.js
@@ -3,15 +3,15 @@
 // @name        	Hide Stack Overflow Sidebar
 // @description 	Hides room wasting/attention diverting sidebars on ALL SO/SE sites
 // @namespace   	https://github.com/allanlaal/hide-stackexchange-sidebars
-// @compatible      	firefox
-// @compatible      	chrome
-// @run-at          	document-start
+// @compatible      firefox
+// @compatible      chrome
+// @run-at          document-start
 // @grant        	none
-// @homepageURL     	https://github.com/allanlaal/hide-stackexchange-sidebars
-// @supportURL      	https://github.com/allanlaal/hide-stackexchange-sidebars/issues
+// @homepageURL     https://github.com/allanlaal/hide-stackexchange-sidebars
+// @supportURL      https://github.com/allanlaal/hide-stackexchange-sidebars/issues
 // @contributionURL	https://github.com/allanlaal/hide-stackexchange-sidebars
 // @author      	inspiration: Machavity ( https://gist.github.com/machavity/420197ff9108e0b757c9bbb7d487f965 )
-// @author		Allan Laal
+// @author			Allan Laal
 // @include    		/^https?:\/\/(.*\.)?stackexchange\.com/.*$/
 // @include    		/^https?:\/\/(.*\.)?stackoverflow\.com/.*$/
 // @include    		/^https?:\/\/stackoverflow\.com/.*$/
