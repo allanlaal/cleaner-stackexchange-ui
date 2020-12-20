60a7d8bcd39864874db04894a236807d721b8a15
Allan Laal <allan.laal@gmail.com>
2020-12-20 16:09:39 +0200

    FIX raw file does not offer UserScript install with TamperMonkey

--- hide-both-sidebars.js
+++ hide-both-sidebars.js
@@ -2,7 +2,6 @@
 // @version     	2020-12-20
 // @name        	Hide Stack Overflow Sidebar
 // @description 	Hides room wasting/attention diverting sidebars on ALL SO/SE sites
-
 // @namespace   	https://github.com/allanlaal/hide-stackexchange-sidebars
 // @compatible      	firefox
 // @compatible      	chrome
@@ -12,10 +11,7 @@
 // @contributionURL	https://github.com/allanlaal/hide-stackexchange-sidebars
 // @author      	inspiration: Machavity ( https://gist.github.com/machavity/420197ff9108e0b757c9bbb7d487f965 )
 // @author		Allan Laal
-
 // @include    		/^https?:\/\/(.*\.)?stackexchange\.com/.*$/
-// run this code at https://stackexchange.com/sites?view=list to get list of custom SE/SO domains: 
-// jQuery('.lv-info a').each(function() {if(jQuery(this).attr("href").indexOf(".stackexchange.com") == -1) console.log(jQuery(this).attr('href'))})
 // @include    		/^https?:\/\/(.*\.)?stackoverflow\.com/.*$/
 // @include    		/^https?:\/\/stackoverflow\.com/.*$/
 // @include    		/^https?:\/\/serverfault\.com/.*$/
