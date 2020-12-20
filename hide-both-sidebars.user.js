52e88e072291c15413c2b37f4dc7b5831cf0e98d
Allan Laal <allan.laal@gmail.com>
2020-12-20 17:04:06 +0200

    FIX hide left sidebar did not work

--- hide-both-sidebars.user.js
+++ hide-both-sidebars.user.js
@@ -1,5 +1,5 @@
 // ==UserScript==
-// @version     	2020-12-20.10
+// @version     	2020-12-20.11
 // @name        	Hide Stack Overflow Sidebar
 // @description 	Hides room wasting/attention diverting sidebars on ALL SO/SE sites
 // @namespace   	https://github.com/allanlaal/hide-stackexchange-sidebars
@@ -10,8 +10,8 @@
 // @homepageURL     https://github.com/allanlaal/hide-stackexchange-sidebars
 // @supportURL      https://github.com/allanlaal/hide-stackexchange-sidebars/issues
 // @contributionURL	https://github.com/allanlaal/hide-stackexchange-sidebars
-// @author      	inspiration: Machavity ( https://gist.github.com/machavity/420197ff9108e0b757c9bbb7d487f965 )
 // @author			Allan Laal
+// @inspiration     Machavity ( https://gist.github.com/machavity/420197ff9108e0b757c9bbb7d487f965 )
 // @include    		/^https?:\/\/(.*\.)?stackexchange\.com/.*$/
 // @include    		/^https?:\/\/(.*\.)?stackoverflow\.com/.*$/
 // @include    		/^https?:\/\/stackoverflow\.com/.*$/
@@ -26,8 +26,11 @@
 (function() {
 	'use strict';
 	
-	document.getElementByClass('left-sidebar').style.display = 'none';
+	document.getElementsByClassName('left-sidebar')[0].style.display = 'none'; // unpinned
+	document.getElementsByClassName('left-sidebar')[1].style.display = 'none'; // pinned
+	
 	document.getElementById('sidebar').style.display = 'none';
+	
 	document.getElementById('mainbar').style = 'width: auto';
 })();
 
