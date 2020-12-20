1395eb7cc76ad7e13a11355a75503a33abc2b83c
Allan Laal <allan.laal@gmail.com>
2020-12-20 17:15:39 +0200

    Update hide-both-sidebars.user.js

--- hide-both-sidebars.user.js
+++ hide-both-sidebars.user.js
@@ -1,17 +1,17 @@
 // ==UserScript==
-// @version     	2020-12-20.11
+// @version     	2020-12-20.14
 // @name        	Hide Stack Overflow Sidebar
 // @description 	Hides room wasting/attention diverting sidebars on ALL SO/SE sites
 // @namespace   	https://github.com/allanlaal/hide-stackexchange-sidebars
-// @compatible      firefox
-// @compatible      chrome
-// @run-at          document-start
+// @compatible      	firefox
+// @compatible      	chrome
+// @run-at          	document-start
 // @grant        	none
-// @homepageURL     https://github.com/allanlaal/hide-stackexchange-sidebars
-// @supportURL      https://github.com/allanlaal/hide-stackexchange-sidebars/issues
+// @homepageURL     	https://github.com/allanlaal/hide-stackexchange-sidebars
+// @supportURL      	https://github.com/allanlaal/hide-stackexchange-sidebars/issues
 // @contributionURL	https://github.com/allanlaal/hide-stackexchange-sidebars
-// @author			Allan Laal
-// @inspiration     Machavity ( https://gist.github.com/machavity/420197ff9108e0b757c9bbb7d487f965 )
+// @author		Allan Laal
+// @inspiration     	Machavity ( https://gist.github.com/machavity/420197ff9108e0b757c9bbb7d487f965 )
 // @include    		/^https?:\/\/(.*\.)?stackexchange\.com/.*$/
 // @include    		/^https?:\/\/(.*\.)?stackoverflow\.com/.*$/
 // @include    		/^https?:\/\/stackoverflow\.com/.*$/
@@ -26,12 +26,17 @@
 (function() {
 	'use strict';
 	
+	// left sidebar: 
 	document.getElementsByClassName('left-sidebar')[0].style.display = 'none'; // unpinned
 	document.getElementsByClassName('left-sidebar')[1].style.display = 'none'; // pinned
 	
+	// right sidebar:
 	document.getElementById('sidebar').style.display = 'none';
 	
+	// top Hero/spam banner:
 	document.getElementsByClassName('js-dismissable-hero')[0].style.display = 'none'; 
+	
+	// make main content area use room we just won:
 	document.getElementById('mainbar').style = 'width: auto';
 })();
 
