c52bc0449efe8ae2754f6cda6641a9f961279b12
Allan Laal <allan.laal@gmail.com>
2020-12-21 02:36:21 +0200

    FIX regression

--- clean-SE-UI.user.js
+++ clean-SE-UI.user.js
@@ -1,31 +1,29 @@
 // ==UserScript==
-// @version     	2020-12-20.16
-// @name        	Hide Stack Overflow Sidebar
+// @version     	2020-12-21.7
+// @name        	Cleaner UI for SE/SO sites
 // @description 	Hides room wasting/attention diverting sidebars on ALL SO/SE sites
 // @namespace   	https://github.com/allanlaal/hide-stackexchange-sidebars
 // @compatible      	firefox
 // @compatible      	chrome
-// @run-at          	document-start
+// @run-at          	document-end
 // @grant        	none
 // @homepageURL     	https://github.com/allanlaal/hide-stackexchange-sidebars
 // @supportURL      	https://github.com/allanlaal/hide-stackexchange-sidebars/issues
 // @contributionURL	https://github.com/allanlaal/hide-stackexchange-sidebars
 // @author		Allan Laal
 // @inspiration     	Machavity ( https://gist.github.com/machavity/420197ff9108e0b757c9bbb7d487f965 )
-// @include    		/^https?:\/\/(.*\.)?stackexchange\.com/.*$/
-// @include    		/^https?:\/\/(.*\.)?stackoverflow\.com/.*$/
-// @include    		/^https?:\/\/stackoverflow\.com/.*$/
-// @include    		/^https?:\/\/serverfault\.com/.*$/
-// @include    		/^https?:\/\/superuser\.com/.*$/
-// @include    		/^https?:\/\/askubuntu\.com/.*$/
-// @include    		/^https?:\/\/stackapps\.com/.*$/
-// @include    		/^https?:\/\/mathoverflow\.net/.*$/
-// @include    		https://mathoverflow.net/questions/*
+// @include    		https://(.*\.)?stackexchange.com/*
+// @include    		https://(.*\.)?stackoverflow.com/*
+// @include    		https://stackoverflow.com/*
+// @include    		https://serverfault.com/*
+// @include    		https://superuser.com/*
+// @include    		https://askubuntu.com/*
+// @include    		https://stackapps.com/*
+// @include    		https://mathoverflow.net/*
 // @exclude 		*://chat.stackexchange.com/*
 // ==/UserScript==
 
 (function() {
-	'use strict';
 	
 	// hide left sidebar: 
 	document.getElementsByClassName('left-sidebar')[0].style.display = 'none'; // unpinned
